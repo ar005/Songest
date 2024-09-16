c823ac94055387d464761fa0ad8d6ba01142e371
ar005 <108510728+ar005@users.noreply.github.com>
2024-09-16 22:49:18 +0530

    Update download.py
    Added lyrics download

--- download.py
+++ download.py
@@ -8,7 +8,7 @@ link=sys.argv[1]
 
 def download(link,directory):
     os.chdir(directory)
-    final_link="python3 -m spotdl "+link
+    final_link="python3 -m spotdl --lyrics synced "+link
     os.popen(final_link)
     
 directory_name=name(link)
